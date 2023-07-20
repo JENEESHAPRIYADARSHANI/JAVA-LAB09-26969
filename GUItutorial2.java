e8885eead325b2f5fd2499d823e051ee55b0ec97
Jeneesha Priyadarshani <135360330+JENEESHAPRIYADARSHANI@users.noreply.github.com>
2023-07-20 11:13:18 +0530

    Add files via upload

---
+++ GUItutorial2.java
@@ -0,0 +1,12 @@
+package com.mycompany.guitutorial2;
+
+public class GUItutorial2 {
+
+    public static void main(String[] args) 
+    {
+      
+      Count C1=new Count();
+      C1.show();
+      
+    }
+}
